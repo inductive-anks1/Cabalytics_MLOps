efa6a623ec0e35197a1ed863d0a8266420dd42c7
Ankit <ankit.kumar@plaksha.edu.in>
2024-01-22 15:29:40 +0530

    Train model path problem. Trying to resolve

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -3,6 +3,9 @@ import pathlib
 import sys
 import joblib
 import mlflow
+import pickle
+import yaml
+
 
 import pandas as pd
 from hyperopt import hp
@@ -23,3 +26,51 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor,GradientBoostingRegressor,AdaBoostRegressor,ExtraTreesRegressor
 from sklearn.svm import SVR
 from xgboost import XGBRegressor
+
+def train_model(X_train, y_train, n_estimators, max_depth, max_samples, max_features, seed):
+    # Define the preprocessing and model steps
+    step1 = ColumnTransformer(transformers=[
+        ('col_tnf', OneHotEncoder(sparse=False, drop='first'), [0, 1, 2, 3])
+    ], remainder='passthrough')
+
+    step2 = RandomForestRegressor(n_estimators=n_estimators,
+                                  random_state=seed,
+                                  max_samples=max_samples,
+                                  max_features=max_features,
+                                  max_depth=max_depth)
+
+    # Create the pipeline
+    pipe = Pipeline([
+        ('step1', step1),
+        ('step2', step2)
+    ])
+
+    # Fit the model
+    pipe.fit(X_train, y_train)
+    return pipe
+
+def save_model(model, output_path):
+    # Save the trained model to the specified output path
+    pickle.dump(model, output_path + '/model.joblib')
+
+def main():
+    curr_dir = pathlib.Path(__file__)
+    home_dir = curr_dir.parent.parent.parent
+    params_file = home_dir.as_posix() + '/params.yaml'
+    params = yaml.safe_load(open(params_file))["train_model"]
+
+    input_file = sys.argv[1]
+    data_path = home_dir.as_posix() + input_file
+    output_path = home_dir.as_posix() + '/models'
+    pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
+    
+    train_features = pd.read_csv(data_path + '/Cab_Data_Train.csv')
+    X_train = train_features.drop('Cab_Price', axis=1)
+    y_train = train_features['Cab_Price']
+
+    trained_model = train_model(X_train, y_train, params['n_estimators'], params['max_depth'], 
+                                params['max_samples'], params['max_features'], params['seed'])
+    save_model(trained_model, output_path)
+
+if __name__ == "__main__":
+    main()
